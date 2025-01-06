1c9820175b4ef2425cea766cd68849ef10ce9b38
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-01-07 00:19:27 +0330

    chore: add page layout

---
+++ src/components/page-layout/page-layout.styles.js
@@ -0,0 +1,23 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: "2rem",
+    color: theme.colorPalette.black,
+    flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+  },
+}));
+
+export default useStyles;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,11 @@
+import Page from "../../components/page-layout";
+
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <Page title="Dashboard" actions={<div>Actions</div>}>
+      Dashboard Page Content
+    </Page>
+  );
 };
 
 export default Dashboard;
