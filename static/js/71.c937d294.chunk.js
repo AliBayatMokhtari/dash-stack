be7ba1c0dc43b1980371830b4862519b030cddbc
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-01-07 23:17:58 +0330

    Updates

--- static/js/71.c937d294.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact=self.webpackChunkreact||[]).push([[71],{71:(e,a,s)=>{s.r(a),s.d(a,{default:()=>u});var c=s(216),r=s(428),t=s(579);const u=()=>(0,t.jsx)(c.C5,{to:r.A.dashboard,replace:!0})}}]);
-//# sourceMappingURL=71.c937d294.chunk.js.map
