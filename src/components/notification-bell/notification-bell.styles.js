b71661cec401cb4b89a7a8f65ab21d79e96d3970
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-01-14 13:45:44 +0330

    chore: add popover

--- src/components/notification-bell/notification-bell.styles.js
+++ src/components/notification-bell/notification-bell.styles.js
@@ -1,9 +1,24 @@
 import { createUseStyles } from "react-jss";
 
-const useStyles = createUseStyles({
+const useStyles = createUseStyles((theme) => ({
   container: {
     cursor: "pointer",
   },
-});
+  popoverContainer: {
+    backgroundColor: theme.colorPalette.white,
+    borderRadius: "0.875rem",
+    paddingBlock: "0.875rem",
+    width: "15.875rem",
+    filter: `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))`,
+  },
+  notification: {
+    fontWeight: 600,
+    fontSize: "0.9375rem",
+    color: theme.colorPalette.darkGray,
+    paddingBottom: "0.8125rem",
+    paddingInline: "1.25rem",
+    borderBottom: `1px solid ${theme.colorPalette.divider}`,
+  },
+}));
 
 export default useStyles;
